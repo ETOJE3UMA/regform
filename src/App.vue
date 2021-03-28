5b1f61220b9418739906bf0f3aa850e1a61a5ccd
Anastasia <ms.suprex@mail.ru>
2021-03-28 14:39:23 +0700

    Проверки работают

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form @submit.prevent="someAction()">
+    <div class="field">
+      <label for="name">Имя</label>
+      <input
+        v-model="name"
+        type="name"
+        @blur="isNameTouched = true"
+        :class="{ error: isNameError }"
+      >
+      <div v-if="isNameError">Введено не корректное значение</div>
+    </div>
+    <div class="field">
+      <label for="email">Email</label>
+      <input
+        v-model="email"
+        type="email"
+        @blur="isEmailTouched = true"
+        :class="{ error: isEmailError }"
+      >
+      <div v-if="isEmailError">Введено не корректное значение</div>
+    </div>
+    <div class="field">
+      <label for="tel">Телефон</label>
+      <input
+        v-model="tel"
+        type="tel"
+        @blur="isTelTouched = true"
+        :class="{ error: isTelError }"
+      >
+      <div v-if="isTelError">Введено не корректное значение</div>
+    </div>
+
+    <button type="submit" :disabled="!isFormValid">
+      Отправить форму
+    </button>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//eslint-disable-next-line
+const emailCheckRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+//eslint-disable-next-line
+const nameCheckRegex = /^[a-zа-я]+(-[a-zа-я]+( [a-zа-я]+)?||( [a-zа-я]+)(-[a-zа-я]+)?)?$/i;
+//eslint-disable-next-line
+const telCheckRegex = /^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/;
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      email: null,
+      isEmailTouched: false,
+      name: null,
+      isNameTouched: false,
+      tel: null,
+      isTelTouched: false
+    };
+  },
+
+  computed: {
+    //Проверка электронной почты
+    isEmailValid() {
+      return emailCheckRegex.test(this.email);
+    },
+    isEmailError() {
+      return !this.isEmailValid && this.isEmailTouched;
+    },
+    //Проверка имени
+    isNameValid() {
+      return nameCheckRegex.test(this.name);
+    },
+    isNameError() {
+      return !this.isNameValid && this.isNameTouched;
+    },
+    //Проверка телефона
+    isTelValid() {
+      return telCheckRegex.test(this.tel);
+    },
+    isTelError() {
+      return !this.isTelValid && this.isTelTouched;
+    },
+    //Форма заполнена правильно
+    isFormValid() {
+      return this.isEmailValid && this.isNameValid && this.isTelValid;
+    }
+  },
+  methods: {
+    someAction() {
+      if (!this.isEmailValid && !this.isNameValid && !this.isTelValid) {
+        return;
+      }
+      alert("Форма отправлена");
+    },
+  },
+};
 </script>
 
 <style>
